11eb0f0a1dcaf4ae493c177a2ea57d6c5dd3a6ce
Max <maximilian.kuchlbauer@gmail.com>
2025-03-05 16:48:56 +0100

    Updates

---
+++ static/js/198.6db01f89.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfitness_tracker=self.webpackChunkfitness_tracker||[]).push([[198],{1198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.6db01f89.chunk.js.map
